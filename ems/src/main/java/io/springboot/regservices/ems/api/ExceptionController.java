080bfc4f33fba9840503479c3a2d7ca0cbf76541
jm01012021 <jm26111984@gmail.com>
2021-02-07 06:11:31 +0000

    added controllers

---
+++ ems/src/main/java/io/springboot/regservices/ems/api/ExceptionController.java
@@ -0,0 +1,24 @@
+package io.springboot.regservices.ems.api;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.springboot.regservices.ems.service.Exception;
+
+@RestController
+public class ExceptionController {
+    
+    @RequestMapping("/exceptions")
+    public List<Exception> getAllExceptions(){
+        return Arrays.asList(
+            new Exception("01/03/2021","1000","01/04/2021 00:25:12:10","MENO","01032021MENOFIRMID","UUID1","100","10.1"),
+            new Exception("01/03/2021","1000","01/04/2021 00:25:12:10","MENO","01032021MENOFIRMID","UUID1","100","10.1"),        
+            new Exception("01/03/2021","1000","01/04/2021 00:25:12:10","MENO","01032021MENOFIRMID","UUID1","100","10.1"),
+            new Exception("01/03/2021","1000","01/04/2021 00:25:12:10","MENO","01032021MENOFIRMID","UUID1","100","10.1"),         
+            new Exception("01/03/2021","1000","01/04/2021 00:25:12:10","MENO","01032021MENOFIRMID","UUID1","100","10.1")
+            
+            );
+    }
+
+
+}
---
+++ ems/src/main/java/io/springboot/regservices/ems/service/Exception.java
@@ -0,0 +1,101 @@
+package io.springboot.regservices.ems.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Exception {
+    
+    private String runDate;
+    private String errorCode;
+    private String timeStamp;
+    private String eventType;
+    private String firmRoeId;
+    private String txnKey;
+    private String quantity;
+    private String price;
+
+    public Exception(){
+
+    }
+    
+    public Exception(String runDate, String errorCode, String timeStamp, String eventType, String firmRoeId,
+     String txnKey, String quantity, String price) {
+        this.runDate = runDate;
+        this.errorCode = errorCode;
+        this.timeStamp = timeStamp;
+        this.eventType = eventType;
+        this.firmRoeId = firmRoeId;
+        this.txnKey = txnKey;
+        this.quantity = quantity;
+        this.price = price;
+
+    }
+
+    public String getRunDate() {
+        return runDate;
+    }
+
+    public void setRunDate(String runDate) {
+        this.runDate = runDate;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getFirmRoeId() {
+        return firmRoeId;
+    }
+
+    public void setFirmRoeId(String firmRoeId) {
+        this.firmRoeId = firmRoeId;
+    }
+
+    public String getTxnKey() {
+        return txnKey;
+    }
+
+    public void setTxnKey(String txnKey) {
+        this.txnKey = txnKey;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+   
+    
+
+}
